22ef617d39d6389e2c956e548111057d980b67e9
SanjidArsann <moshiurrahmanm3@gmail.com>
2023-12-09 12:52:35 +0600

    update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import ActiveLink from "./ActiveLink";
 import '../Style/ActiveLink.css'
-import { ChevronDownIcon} from '@heroicons/react/24/solid'
+import { ChevronDownIcon} from '@heroicons/react/24/solid';
+import LogoOne from '../../../public/logo-1.png';
+import Dot from '../../../public/dot.png'
 
 const Header = () => {
 
@@ -11,7 +13,7 @@ const Header = () => {
 
     <div className="flex justify-between align-middle px-16 bg-cover bg-center bg-gradient-to-r from-slate-50 via-slate-50 to-white  ">
       <div className="flex align-middle justify-center py-6 ">
-        <img className="h-11" src="../../../public/logo-1.png" alt="" />
+        <img className="h-11" src={LogoOne}alt="LogoOne" />
       </div>
 
       <div className="flex align-middle">
@@ -42,7 +44,7 @@ const Header = () => {
       </div>
         <div className=" my-6 rounded-md flex  justify-start align-middle bg-indigo-600">
         <a href="" className=" p-4">
-          <img className=" " src="../../../public/dot.png" alt="" />
+          <img className=" " src={Dot} alt="Dot" />
         </a>
       </div>
       
