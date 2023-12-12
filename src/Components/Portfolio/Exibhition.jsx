f3dcb28cbac2959a32522618d8a32fa467d88164
SanjidArsann <moshiurrahmanm3@gmail.com>
2023-12-12 16:58:39 +0600

    update

--- src/Components/Portfolio/Exibhition.jsx
+++ src/Components/Portfolio/Exibhition.jsx
@@ -5,7 +5,7 @@ const Exibhition = ({ project }) => {
     <div>
       <div className="Cart">
         <div className="relative group">
-          <img src={project.image} alt="ImageOne" />
+          <img src={project.image} className="w-full" alt="ImageOne" />
           <div className="absolute inset-0 bg-indigo-800 opacity-0 group-hover:opacity-50 transition-opacity duration-500"></div>
           <div className="absolute inset-0 flex items-center justify-center overflow-hidden">
             <span className="text-white text-5xl font-semibold transform -translate-y-full opacity-0 group-hover:opacity-100 group-hover:translate-y-0 transition-transform duration-500">
